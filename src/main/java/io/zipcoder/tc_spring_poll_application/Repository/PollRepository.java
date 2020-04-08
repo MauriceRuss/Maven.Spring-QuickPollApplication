51f402d746ac84d728d487ca8d639bbc5eaa722f
Mo Russ <mauriceruss302@gmail.com>
2020-04-07 22:45:07 -0400

    OptionRep PollRepo and VoteRepo created

---
+++ src/main/java/io/zipcoder/tc_spring_poll_application/Repository/PollRepository.java
@@ -0,0 +1,8 @@
+package io.zipcoder.tc_spring_poll_application.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PollRepository extends CrudRepository {
+}
